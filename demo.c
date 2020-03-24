661b01a2f22f94dacd551341ce02e5ec9190c7c4
lucas <zhang010725@gmail.com>
2020-03-24 17:25:20 +0800

    .gitignore added .vscode/* and demo.exe deleted

--- demo.c
+++ demo.c
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
     SetConsoleOutputCP(65001); //设置输出编码为utf-8
-    dataframe df = read_csv("test.csv"); //从test读取数据
+    dataframe df = read_csv("../test.csv"); //从test读取数据
     dfprint(&df,1); //以预览模式输出 df
     operate_trans(&df,dfTRUE); //将df转化为可统计对象
     cell sum = dfsum(&df,0); // 获取df第一列的和
